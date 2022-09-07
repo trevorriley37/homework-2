557b4b92dac2607dc223548a3f774f30122c0e6f
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-09-07 14:26:46 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,8 +1,8 @@
 <?php require_once("header.php"); ?>
 
 
-
-Welcome <?php echo $_GET["name"]; ?><br>
+<h1>Post response</h1>
+    Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 
 
