e44b6c192fbf2c9e21e4ea8eb3fa0146344f25e1
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-09-07 14:40:16 -0500

    Update post.php

--- post.php
+++ post.php
@@ -12,8 +12,8 @@
     <h1>Post Response</h1>
     
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
 
 
