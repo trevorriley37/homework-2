dbd84132ed79370bfd6a99729f3ed8752afba2de
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-09-12 13:53:27 -0500

    Update and rename get.php to getcollection.php

---
+++ getcollection.php
@@ -0,0 +1,10 @@
+<?php require_once("header.php"); ?>
+
+
+<form action="post.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+<?php require_once("footer.php"); ?>
